81a73cc384715fcd1dc4ade5ff2d08a8cf7d35a9
CCLM2020 <lujan_maldonado@yahoo.com.ar>
2023-09-05 12:04:37 -0300

    martes cambios

--- Practico_Lunes_04_09/js/main.js
+++ Practico_Lunes_04_09/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('#nota_01, #nota_02, #nota_03').inputmask({
         suffix: '',
         alias: "numeric",
-        placeholder: "0",
+        placeholder: "",
         autoGroup: true,
         integerDigits: 2,
         digits: 0,
@@ -28,76 +28,48 @@ $(document).ready(function () {
     //cuando hago click en el boton agregar alumno
     $('#btn_agregar_alumno').click(function () {
 
+        if (ComprobarDatos()) {
+        
+        }
+        /*
         let nombre_estudiante = $('#nombre_estudiante').val();
         let notas_estudiante = [parseFloat($('#nota_01').val()), parseFloat($('#nota_02').val()), parseFloat($('#nota_03').val())];
         
         estudiantes.push(new estudiante(nombre_estudiante, notas_estudiante));
+
+        alert($('#nota_01').val())
         if (validateNumero($('#nota_01').val())) {
             alert('funciona')
         } else {
             alert('noooo')
         }
-        console.log(estudiantes);
+        console.log(estudiantes);*/
+
     });
 
 
     //Esta función comprueba los datos requeridos
     function ComprobarDatos() {
         if ($('#nombre_estudiante').val() === "") {
-            $('#nombre_estudiante').css("background", "#FDB0AF");
+            //$('#nombre_estudiante').css("background", "#FDB0AF");
+            $('#nombre_estudiante').addClass('error');
             $('#nombre_estudiante').attr("placeholder", "Falta el nombre del alumno");
             $("#nombre_estudiante").focus();
             return false;
-        } else if ($('#id_txt_apellido').val() === "") {
-            $('#id_txt_apellido').css("background", "#FDB0AF");
-            $('#id_txt_apellido').attr("placeholder", "Falta el apellido del usuario");
-            $("#id_txt_apellido").focus();
+        } else if ($('#nota_01').val() === "" || !validateNumero($('#nota_01').val())) {
+            $('#nota_01').css("background", "#FDB0AF");
+            $('#nota_01').attr("placeholder", "");
+            $("#nota_01").focus();
             return false;
-        } else if (!checkEdad('id_txt_edad', 'span_error_edad')) {
-            $('#span_error_edad').removeClass('correo-hide');
-            $('#id_txt_edad').css("background", "#FDB0AF");
-            $("#id_txt_edad").focus();
+        } else if ($('#nota_02').val() === "" || !validateNumero($('#nota_02').val())) {
+            $('#nota_02').removeClass('correo-hide');
+            $('#nota_02').css("background", "#FDB0AF");
+            $("#nota_02").focus();
             return false;
-        } else if ($('#id_txt_correo').val() == "") {
-            $('#id_txt_correo').css("background", "#FDB0AF");
-            $('#id_txt_correo').attr("placeholder", "Falta ingresar el email");
-            $("#id_txt_correo").focus();
-            return false;
-        } else if (!validarFormatoEmail('id_txt_correo')) {
-            $('#span_error_Email').text('Utilice un formato de correo válido (ejemplo: miCorreo@correo.com).');
-            $('#span_error_Email').removeClass('correo-hide');
-            $('#id_txt_correo').css("background", "#FDB0AF");
-            $("#id_txt_correo").focus();
-            return false;
-        } else if ($('#id_txt_usuario').val() == "") {
-            $('#id_txt_usuario').css("background", "#FDB0AF");
-            $('#id_txt_usuario').attr("placeholder", "Falta el apellido del usuario");
-            $("#id_txt_usuario").focus();
-            return false;
-        } else if ($('#id_txt_password_1').val() == "") {
-            $('#span_error_Pass_A').text('Falta ingresar la contraseña');
-            $('#span_error_Pass_A').removeClass('correo-hide');
-            $('#id_txt_password_1').css("background", "#FDB0AF");
-            $("#id_txt_password_1").focus();
-            return false;
-        } else if ($('#id_txt_password_2').val() == "") {
-            $('#span_error_Pass_B').text('Falta ingresar la contraseña');
-            $('#span_error_Pass_B').removeClass('correo-hide');
-            $('#id_txt_password_2').css("background", "#FDB0AF");
-            $("#id_txt_password_2").focus();
-            return false;
-        } else if (!comprobarIgualdad('id_txt_password_1', 'id_txt_password_2')) {
-            $('#span_error_Pass_A').text('Las contraseñas son distintas');
-            $('#span_error_Pass_A').removeClass('correo-hide');
-            $('#id_txt_password_1').css("background", "#FDB0AF");
-            $('#id_txt_password_2').css("background", "#FDB0AF");
-            $("#id_txt_password_1").focus();
-            return false;
-        } else if (!checkPassword('id_txt_password_1', 'span_error_Pass_A')) {
-            $('#span_error_Pass_A').removeClass('correo-hide');
-            $('#id_txt_password_1').css("background", "#FDB0AF");
-            $('#id_txt_password_2').css("background", "#FDB0AF");
-            $("#id_txt_password_1").focus();
+        } else if ($('#nota_03').val() === "" || !validateNumero($('#nota_03').val())) {
+            $('#nota_03').css("background", "#FDB0AF");
+            $('#nota_03').attr("placeholder", "");
+            $("#nota_03").focus();
             return false;
         } else {
             return true;
