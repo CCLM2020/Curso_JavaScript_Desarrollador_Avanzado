a82a999caee4074e008c17510b6d18add17fa538
CCLM2020 <lujan_maldonado@yahoo.com.ar>
2023-09-11 20:53:50 -0300

    nuevo de javascript

---
+++ objetos/objetos.js
@@ -0,0 +1,34 @@
+//objetos
+/*
+const MediosComunicacion = {
+    saludar: function () {
+        console.log("¡Hola desde MediosComunicacion!");
+    }
+};
+
+
+const radio1 = Object.create(MediosComunicacion);
+
+const radio2 = Object.create(MediosComunicacion);
+
+console.log('Este es radio1: ');
+radio1.saludar();
+
+console.log('Este es radio2: ');
+radio1.saludar();
+*/
+
+
+//clases
+/*
+Necesito almacenar información de Clientes, los mismos pueden ser : consumidores finales, responsables inscriptos, pyme
+Necesito almacenar la información de ellos : nombre, dirección, mail, telefono
+Pero en el caso de consumidor final guardar:
+Apellido, DNI
+En el caso de responsable inscripto: CUIT, ingresos brutos, numero de punto de venta
+Si es el caso de una pyme almacenar : nombre de fantasía, CUIT
+Debo poder mostrar el saldo deudor de cualquiera de mis clientes
+Si es responsable quiero ver el cuit
+Si es consumidor final quiero ver el dni
+Y si es pyme quiero ver el nombre de fantasua
+*/
