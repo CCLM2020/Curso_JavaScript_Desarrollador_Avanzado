2e31926bc406b554dd69563445a71f7103f43341
CCLM2020 <lujan_maldonado@yahoo.com.ar>
2023-09-18 18:42:39 -0300

    nuevo trabajo jquery

---
+++ Ejercicio_Jquery/js/main.js
@@ -0,0 +1,142 @@
+$(document).ready(function () {
+    // Clase productos
+    class Producto {
+        constructor(nombre, precio, cantidadEnStock) {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.cantidadEnStock = cantidadEnStock;
+        }
+
+        // metodo para mostrar el producto
+        mostrarInformacion() {
+            console.log(`Nombre: ${this.nombre}`);
+            console.log(`Precio: ${this.precio}`);
+            console.log(`Cantidad en stock: ${this.cantidadEnStock}`);
+        }
+    }
+
+    // clase hija para productos electronicos
+    class ProductoElectronico extends Producto {
+        constructor(nombre, precio, cantidadEnStock, potencia) {
+            super(nombre, precio, cantidadEnStock);
+            this.potencia = potencia;
+        }
+
+        // metodo para mostrar un producto electronico
+        mostrarInformacion() {
+            super.mostrarInformacion();
+            console.log(`Potencia: ${this.potencia}`);
+        }
+    }
+
+    // Clase hija para productos alimenticio
+    class ProductoAlimenticio extends Producto {
+        constructor(nombre, precio, cantidadEnStock, fechaCaducidad) {
+            super(nombre, precio, cantidadEnStock);
+            this.fechaCaducidad = fechaCaducidad;
+        }
+
+        // metodo para mostrar un producto alimenticio
+        mostrarInformacion() {
+            super.mostrarInformacion();
+            console.log(`Fecha de caducidad: ${this.fechaCaducidad}`);
+        }
+    }
+
+    // Clase Carrito
+    class Carrito {
+        constructor() {
+            this.productos = [];
+        }
+
+        // Metodo para agregar un producto al carrito
+        agregarProducto(producto) {
+            this.productos.push(producto);
+        }
+
+        // mtodo para eliminar un producto del carrito
+        eliminarProducto(producto) {
+            for (let i = 0; i < this.productos.length; i++) {
+                if (this.productos[i] === producto) {
+                    this.productos.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        // Metodo para calcular el total de la compra
+        calcularTotal() {
+            return this.productos.reduce((total, producto) => total + producto.precio, 0);
+        }
+    }
+
+    // nuevo carrito
+    const carrito = new Carrito();
+
+    // agregamos productos al carrito
+    //carrito.agregarProducto(productoElectronico);
+    //carrito.agregarProducto(productoAlimenticio);
+
+    // Mostrarmos el total de la compra
+    //console.log('Monto total en el carrito: $' + carrito.calcularTotal());
+
+    // creando array para productos
+    let listadoProductos = [];
+
+    listadoProductos.push(new ProductoElectronico("Smart TV Hyundai 50'' 4k", 209000, 10, 100));
+    listadoProductos.push(new ProductoElectronico("Smart TV Hitachi 55'' 4k", 203000, 8, 150));
+    listadoProductos.push(new ProductoElectronico("Android Tv Philips Led Full Hd 43", 159999, 12, 120));
+    listadoProductos.push(new ProductoAlimenticio("Leche Entera Larga Vida X 1 L", 499, 20, "2024-09-30"));
+    listadoProductos.push(new ProductoAlimenticio("Ferrero Rocher Bombones", 3699, 15, "2024-10-30"));
+    listadoProductos.push(new ProductoAlimenticio("Te En Saquitos La Virginia Clasico", 807, 55, "2025-11-23"));
+
+    //console.log (listadoProductos)
+
+
+    let card = "";
+    for (let i = 0; i < listadoProductos.length; i++) {
+
+        card = '<div class="col" id="' + i + '">' +
+            '<div class="card h-100 card-shadow">' +
+            '<img src="./imgProductos/' + i + '.png" class="card-img-top img-fluid" alt="..." id="' + i + '.png"><div class="card-body">' +
+            '<div class="card-body">' +
+            '<h5 class="card-title pb-2">' + listadoProductos[i].nombre + '</h5>' +
+            '<p class="card-text">Precio: <span class="text-success">$' + listadoProductos[i].precio + '</span></p>' +
+            '<p class="card-text">Cantidad: <span class="text-primary">' + listadoProductos[i].cantidadEnStock + '</span></p>' +
+            '</div>' +
+            '<div class="text-end p-1">' +
+            '<a class="btn btn-danger agregar-carrito"><i class="fa-solid fa-plus"></i> Agregar carrito</a>' +
+            '</div>' +
+            '</div>'
+        //console.log(card)
+        $('#listado_productos').append(card);
+    }
+
+    $('.agregar-carrito').on('click', function () {
+
+        // Obtenemos el id de la columna clase col mas cercana del boton donde hice clic
+        let col_Id = $(this).closest('.col').attr('id');
+
+        if (listadoProductos[col_Id].cantidadEnStock > 0) {
+
+            // Comprobar si el producto es de la clase ProductoAlimenticio
+            if (listadoProductos[0] instanceof ProductoAlimenticio) {
+                console.log("El producto es un producto alimenticio");
+            } else {
+                console.log("El producto no es un producto alimenticio");
+            }
+
+
+            //let agregarProducto = {id: col_Id, nombre: listadoProductos[col_Id].nombre, precio: listadoProductos[col_Id].precio, cantidad: 1};
+            carrito.agregarProducto(listadoProductos[col_Id]);
+
+
+        }
+        //console.log('ID de la columna: ' + listadoProductos[col_Id].nombre);
+
+        console.log('Monto total en el carrito: $' + carrito.calcularTotal());
+
+    });
+
+});
+
--- objetos/objetos.js
+++ objetos/objetos.js
@@ -32,3 +32,95 @@ Si es responsable quiero ver el cuit
 Si es consumidor final quiero ver el dni
 Y si es pyme quiero ver el nombre de fantasua
 */
+
+// Clase productos
+class Producto {
+    constructor(nombre, precio, cantidadEnStock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadEnStock = cantidadEnStock;
+    }
+
+    // metodo para mostrar el producto
+    mostrarInformacion() {
+        console.log(`Nombre: ${this.nombre}`);
+        console.log(`Precio: ${this.precio}`);
+        console.log(`Cantidad en stock: ${this.cantidadEnStock}`);
+    }
+}
+
+// clase hija para productos electronicos
+class ProductoElectronico extends Producto {
+    constructor(nombre, precio, cantidadEnStock, potencia) {
+        super(nombre, precio, cantidadEnStock);
+        this.potencia = potencia;
+    }
+
+    // metodo para mostrar un producto electronico
+    mostrarInformacion() {
+        super.mostrarInformacion();
+        console.log(`Potencia: ${this.potencia}`);
+    }
+}
+
+// Clase hija para productos alimenticio
+class ProductoAlimenticio extends Producto {
+    constructor(nombre, precio, cantidadEnStock, fechaCaducidad) {
+        super(nombre, precio, cantidadEnStock);
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    // metodo para mostrar un producto alimenticio
+    mostrarInformacion() {
+        super.mostrarInformacion();
+        console.log(`Fecha de caducidad: ${this.fechaCaducidad}`);
+    }
+}
+
+// creando objetos de producto
+const productoElectronico = new ProductoElectronico("Televisión", 1000, 10, 100);
+const productoAlimenticio = new ProductoAlimenticio("Leche", 450, 20, "2023-09-30");
+
+// Mostrar informacion de los productos
+productoElectronico.mostrarInformacion();
+console.log('\n')
+productoAlimenticio.mostrarInformacion();
+console.log('\n')
+
+// Clase Carrito
+class Carrito {
+    constructor() {
+        this.productos = [];
+    }
+
+    // Metodo para agregar un producto al carrito
+    agregarProducto(producto) {
+        this.productos.push(producto);
+    }
+
+    // mtodo para eliminar un producto del carrito
+    eliminarProducto(producto) {
+        for (let i = 0; i < this.productos.length; i++) {
+            if (this.productos[i] === producto) {
+                this.productos.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    // Metodo para calcular el total de la compra
+    calcularTotal() {
+        return this.productos.reduce((total, producto) => total + producto.precio, 0);
+    }
+}
+
+// nuevo carrito
+const carrito = new Carrito();
+
+// agregamos productos al carrito
+carrito.agregarProducto(productoElectronico);
+carrito.agregarProducto(productoAlimenticio);
+
+// Mostrarmos el total de la compra
+console.log('Monto total en el carrito: $' + carrito.calcularTotal());
+console.log(carrito);
