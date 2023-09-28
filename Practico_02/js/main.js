0a2fccfcc4228a9f352fa64bdd0e784d7f4b5f70
CCLM2020 <lujan_maldonado@yahoo.com.ar>
2023-09-28 20:37:53 -0300

    practico 2

---
+++ Practico_02/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+//ejercicio 1
+// Seleccionar el primer párrafo hijo dentro de .contenedor y cambiar su color de texto a rojo
+$(".contenedor p:first").css("color", "red");
+
+// Seleccionar el último párrafo hijo dentro de .contenedor y cambiar su color de texto a rojo
+$(".contenedor p:last").css("color", "red");
+
+});
